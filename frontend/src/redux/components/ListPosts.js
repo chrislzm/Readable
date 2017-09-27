160809953dd05ec75d6483e14daad7f3030de275
chrislzm <me@chrisleung.com>
2017-09-27 15:10:34 -0700

    feat: Added logic to retrieve category name from store, or use default

--- frontend/src/redux/components/ListPosts.js
+++ frontend/src/redux/components/ListPosts.js
@@ -1,26 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import { Link, Route, withRouter } from 'react-router-dom'
+import * as Constants from '../../constants'
+import { connect } from 'react-redux'
 
-const ListPosts = (props) => {
-  let {category} = props.match.params
-  if(!category) {
-    category = "All Posts"
+
+class ListPosts extends Component {
+  render() {
+
+    let {categoryPath} = this.props.match.params
+    let categoryName
+
+    // If a path to a category wasn't defined, or we haven't loaded categories from the server yet
+    if(!categoryPath || this.props.categories.length == 0) {
+      // Assign default category and path
+      categoryPath = Constants.DEFAULT_CATEGORY_PATH
+      categoryName = Constants.DEFAULT_CATEGORY_NAME
+    } else {
+      // Otherwise look up the category name based on the path
+      categoryName = this.props.categories.find(category => category.path === categoryPath).name
+    }
+
+    return(
+      <div>
+        <div className="list-posts">
+          <h2>{categoryName}</h2>
+          <div className="list-posts-title">
+            Post Title
+          </div>
+          <div className="list-books-date">
+            Date
+          </div>
+          <div className="list-posts-votes">
+            Votes
+          </div>
+        </div>
+      </div>
+    )
   }
+}
+const mapStateToProps = (state) => ({
+  categories: state.categories
+});
 
-  return(
-    <div className="list-posts">
-      <h2>{category}</h2>
-      <div className="list-posts-title">
-        Post Title
-      </div>
-      <div className="list-books-date">
-        Date
-      </div>
-      <div className="list-posts-votes">
-        Votes
-      </div>
-    </div>
-  )
-};
-
-export default ListPosts
+export default withRouter(connect(mapStateToProps)(ListPosts));
