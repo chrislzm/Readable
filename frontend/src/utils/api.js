141cf99a04b1329117594522ff13171367c5b8e5
chrislzm <me@chrisleung.com>
2017-09-27 14:03:05 -0700

    feat: Added API calls to get all posts, get category posts style: Fixed formatting

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,20 @@ const headers = {
   'Authorization': 'whatever-you-want'
 }
 
-export const getCategories = () =>
-fetch(`${api}/categories`, { headers })
-.then(res => res.json())
-.then(data => data.categories)
+export const getCategories = () => (
+  fetch(`${api}/categories`, { headers })
+  .then(res => res.json())
+  .then(data => data.categories)
+)
+
+export const getAllPosts = () => (
+  fetch(`${api}/posts`, { headers })
+  .then(res => res.json())
+  .then(data => data.posts)
+)
+
+export const getCategoryPosts = (category) => (
+  fetch(`${api}/${category}/posts`, { headers })
+  .then(res => res.json())
+  .then(data => data.posts)
+)
