dbecd6352db323b3987a70a3a977366bc8b16d97
chrislzm <me@chrisleung.com>
2017-09-26 18:45:32 -0700

    feat: Added the redux store+dev tools+thunk and wired it up to our app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import './style/index.css';
 import App from './redux/components/App';
+import reducer from './redux/reducers/'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
+)
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
