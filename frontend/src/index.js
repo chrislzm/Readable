72c41137b252e73df7ebcd295f46c4b8b03b113b
chrislzm <me@chrisleung.com>
2017-09-26 17:47:24 -0700

    feat: added imports for packages we will use

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import './style/index.css';
-import App from './components/App';
+import App from './redux/components/App';
 
 ReactDOM.render(
   <BrowserRouter>
