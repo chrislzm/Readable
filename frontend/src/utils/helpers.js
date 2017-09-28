21a19b17891a6e16e9255f495567feea68994fff
chrislzm <me@chrisleung.com>
2017-09-27 18:11:19 -0700

    feat: Added "global unique identifier" generator

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -21,5 +21,17 @@ export function getCurrentCategoryPathAndName(urlPath,reduxStoreCategories) {
         // Add forward slash to path for proper linking
         categoryPath = '/' + urlPath
       }
+
       return {categoryPath,categoryName}
+}
+
+// https://stackoverflow.com/a/105074/7602403
+export function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
 }
