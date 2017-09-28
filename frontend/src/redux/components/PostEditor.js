a9f476d581e16c2b6d1212c2405cfcb0b662d03c
chrislzm <me@chrisleung.com>
2017-09-27 18:11:41 -0700

    feat: Form now successfully submits data to the API server!

--- frontend/src/redux/components/PostEditor.js
+++ frontend/src/redux/components/PostEditor.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { capitalize } from '../../utils/helpers'
+import serializeForm from 'form-serialize'
+import { addNewPost } from '../../utils/api'
+import { guid } from '../../utils/helpers'
 
 class PostEditor extends Component {
 
-  handleSubmit = () => {
-    // do something
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.idInput.value = guid()
+    this.timestampInput.value = Date.now()
+    const values = serializeForm(e.target, { hash: true })
+    addNewPost(values)
+    console.log(values)
   }
 
   render() {
@@ -13,7 +21,7 @@ class PostEditor extends Component {
       <div>
         <form onSubmit={this.handleSubmit} className="edit-post-form">
           <div className="edit-post-details">
-            <select defaultValue={this.props.categoryName}>
+            <select name="category" onSubmit={this.handleSubmit} defaultValue={this.props.categoryName}>
               {this.props.categories.map(category => (
                 <option
                   value={category.path}
@@ -26,6 +34,8 @@ class PostEditor extends Component {
             <input type="text" name="title" placeholder="Title"/>
             <input type="textarea" name="body" placeholder="Body"/>
             <input type="text" name="author" placeholder="Author"/>
+            <input type="hidden" name="id" ref={(input) => { this.idInput = input;}}/>
+            <input type="hidden" name="timestamp" ref={(input) => { this.timestampInput = input;}}/>
             <button>Post</button>
           </div>
         </form>
