fb8becc82b7e6884077446663e06e30cd3b292d6
chrislzm <me@chrisleung.com>
2017-09-28 16:46:08 -0700

    feat: Set up table structure and style for displaying posts

--- frontend/src/redux/components/ListPosts.js
+++ frontend/src/redux/components/ListPosts.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import '../../style/ListPosts.css'
 
 class ListPosts extends Component {
   render() {
 
     const {categoryPath, categoryName} = this.props
-
+    console.log(categoryPath,categoryName)
     return(
       <div>
-        <Link to={ `createPost${categoryPath}`}>Create New Post</Link>
-        <div className="list-posts">
-          <h2>{categoryName}</h2>
-          <div className="list-posts-title">
-            Post Title
-          </div>
-          <div className="list-books-date">
-            Date
-          </div>
-          <div className="list-posts-votes">
-            Votes
+        <h2>{categoryName}</h2>
+        <Link to={ `createPost/${categoryPath}`}>Create New Post</Link>
+        <div className="divTable">
+          <div className="divTableBody">
+            <div className="divTableRow">
+              <div className="divTableCell">Category</div>
+              <div className="divTableCell">Post Title</div>
+              <div className="divTableCell">Date</div>
+              <div className="divTableCell">Author</div>
+              <div className="divTableCell">Votes</div>
+            </div>
           </div>
         </div>
       </div>
@@ -27,7 +28,8 @@ class ListPosts extends Component {
   }
 }
 const mapStateToProps = (state) => ({
-  categories: state.categories
+  categories: state.categories,
+  posts: state.posts
 });
 
 export default connect(mapStateToProps)(ListPosts);
