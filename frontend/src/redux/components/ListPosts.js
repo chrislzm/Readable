d405e29fc014cec3b00ebc044696c26a872d0396
chrislzm <me@chrisleung.com>
2017-09-28 17:39:24 -0700

    fix: Added key to posts iteration

--- frontend/src/redux/components/ListPosts.js
+++ frontend/src/redux/components/ListPosts.js
@@ -30,7 +30,7 @@ class ListPosts extends Component {
           </div>
           <div className="divTableBody">
             { posts.filter(post => this.postFilter(post.category,categoryName)).map(post => (
-              <div className="divTableRow">
+              <div className="divTableRow" key={post.id}>
                 <div className="divTableCell">{post.category}</div>
                 <div className="divTableCell">{post.title}</div>
                 <div className="divTableCell">{Moment(post.timestamp, "x").format("MM-DD-YYYY hh:mm A")}</div>
