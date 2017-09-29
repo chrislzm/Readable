a304f68dcc4e5302a3cf3f58e40d7efd2e181169
chrislzm <me@chrisleung.com>
2017-09-28 18:53:17 -0700

    feat: Now displaying post detail with style formatting

--- frontend/src/redux/components/ViewPost.js
+++ frontend/src/redux/components/ViewPost.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import '../../style/ViewPost.css'
 
 class ViewPost extends Component {
   render() {
+    const postId = this.props.match.params.postId
+    const post = this.props.posts[postId]
     return (
-      <div>Post detail here.</div>
+      <div className="VieWPost">
+        { post && (
+          <div className="divTable blueTable">
+            <div className="divTableBody">
+              <div className="divTableRow">
+                <div className="divTableCell">Title:</div>
+                <div className="divTableCell">{post.title}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Category:</div>
+                <div className="divTableCell">{post.category}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Author:</div>
+                <div className="divTableCell">{post.author}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Date:</div>
+                <div className="divTableCell">{post.timestamp}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Votes:</div>
+                <div className="divTableCell">{post.voteScore}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableCell">Body:</div>
+                <div className="divTableCell">{post.body}</div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     )
   }
 }
 
-export default connect()(ViewPost);
+const mapStateToProps = (state) => ({
+  posts: state.posts
+})
+
+export default connect(mapStateToProps)(ViewPost);
