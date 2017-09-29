0f819673a5e5f3b13f6f12fab81e962382e0b2c1
chrislzm <me@chrisleung.com>
2017-09-29 15:02:55 -0700

    feat: Added default site-wide date formatting string

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -2,3 +2,4 @@ export const ALL_POSTS_CATEGORY_PATH = ''
 export const ALL_POSTS_CATEGORY_NAME = 'All Posts'
 export const DEFAULT_VOTES = 1
 export const DEFAULT_DELETED_FLAG = false
+export const DEFAULT_DATE_FORMAT = "MM-DD-YYYY hh:mm A"
