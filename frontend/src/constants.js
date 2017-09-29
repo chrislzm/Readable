ffbc46741ae4d41fa729c08404d05dea5d98d866
chrislzm <me@chrisleung.com>
2017-09-28 18:07:02 -0700

    refactor: Renamed default category path/name feat: Added new constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,4 @@
-export const DEFAULT_CATEGORY_PATH = ''
-export const DEFAULT_CATEGORY_NAME = 'All Posts'
+export const ALL_POSTS_CATEGORY_PATH = ''
+export const ALL_POSTS_CATEGORY_NAME = 'All Posts'
+export const DEFAULT_VOTES = 1
+export const DEFAULT_DELETED_FLAG = false
