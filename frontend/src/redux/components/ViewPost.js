0151fa63ddebfa72c5807abbdbfa20f421ed4ba3
chrislzm <me@chrisleung.com>
2017-09-29 15:03:47 -0700

    feat: Displaying formatted date refactor: Rearranged content to match create post page

--- frontend/src/redux/components/ViewPost.js
+++ frontend/src/redux/components/ViewPost.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import Moment from 'moment'
 import { connect } from 'react-redux'
 import '../../style/ViewPost.css'
+import * as Constants from '../../constants'
 
 class ViewPost extends Component {
   render() {
@@ -12,28 +14,28 @@ class ViewPost extends Component {
           <div className="divTable blueTable">
             <div className="divTableBody">
               <div className="divTableRow">
-                <div className="divTableLabel">Title</div>
-                <div className="divTableCell">{post.title}</div>
-              </div>
-              <div className="divTableRow">
                 <div className="divTableLabel">Category</div>
                 <div className="divTableCell">{post.category}</div>
+              </div>
+              <div className="divTableRow">
+                <div className="divTableLabel">Title</div>
+                <div className="divTableCell">{post.title}</div>
               </div>
               <div className="divTableRow">
                 <div className="divTableLabel">Author</div>
                 <div className="divTableCell">{post.author}</div>
               </div>
               <div className="divTableRow">
+                <div className="divTableLabel">Body</div>
+                <div className="divTableCell">{post.body}</div>
+              </div>
+              <div className="divTableRow">
                 <div className="divTableLabel">Date</div>
-                <div className="divTableCell">{post.timestamp}</div>
+                <div className="divTableCell">{Moment(post.timestamp, "x").format(Constants.DEFAULT_DATE_FORMAT)}</div>
               </div>
               <div className="divTableRow">
                 <div className="divTableLabel">Votes</div>
                 <div className="divTableCell">{post.voteScore}</div>
-              </div>
-              <div className="divTableRow">
-                <div className="divTableLabel">Body</div>
-                <div className="divTableCell">{post.body}</div>
               </div>
             </div>
           </div>
