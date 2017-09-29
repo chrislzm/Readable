9f84aaf5e0ac2b322e9d84293618906ea610674a
chrislzm <me@chrisleung.com>
2017-09-28 19:20:19 -0700

    feat: Added skeleton for voting mechanism

--- frontend/src/redux/components/ListPosts.js
+++ frontend/src/redux/components/ListPosts.js
@@ -7,9 +7,25 @@ import Moment from 'moment'
 
 class ListPosts extends Component {
 
-  postFilter(postCategory,categoryName) {
+  filter(postCategory,categoryName) {
     if(categoryName === Constants.ALL_POSTS_CATEGORY_NAME) return true
     return postCategory === categoryName
+  }
+
+  upVote(postId) {
+    alert("Upvoted!")
+  }
+
+  downVote(postId) {
+    alert("Downvoted!")
+  }
+
+  edit(postId) {
+    alert("Editing")
+  }
+
+  delete(postId) {
+    alert("Deleting")
   }
 
   render() {
@@ -26,16 +42,24 @@ class ListPosts extends Component {
               <div className="divTableHead">Date</div>
               <div className="divTableHead">Author</div>
               <div className="divTableHead">Votes</div>
+              <div className="divTableHead">Actions</div>
+
             </div>
           </div>
           <div className="divTableBody">
-            { posts.filter(post => this.postFilter(post.category,categoryName)).map(post => (
+            { posts.filter(post => this.filter(post.category,categoryName)).map(post => (
               <div className="divTableRow" key={post.id}>
                 <div className="divTableCell">{post.category}</div>
                 <div className="divTableCell"><Link to={`/${post.category}/${post.id}`}>{post.title}</Link></div>
                 <div className="divTableCell">{Moment(post.timestamp, "x").format("MM-DD-YYYY hh:mm A")}</div>
                 <div className="divTableCell">{post.author}</div>
                 <div className="divTableCell">{post.voteScore}</div>
+                <div className="divTableCell">
+                  <button onClick={() => this.upVote(post.id)}>Upvote</button>
+                  <button onClick={() => this.downVote(post.id)}>Downvote</button>
+                  <button onClick={() => this.edit(post.id)}>Edit</button>
+                  <button onClick={() => this.delete(post.id)}>Delete</button>
+                </div>
               </div>
             ))}
           </div>
