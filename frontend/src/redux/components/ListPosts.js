409c3b68d4d21222aafea3fba4fee39b9e305df2
chrislzm <me@chrisleung.com>
2017-09-29 15:21:19 -0700

    refactor: Moved post actions to its own component

--- frontend/src/redux/components/ListPosts.js
+++ frontend/src/redux/components/ListPosts.js
@@ -4,32 +4,13 @@ import { connect } from 'react-redux'
 import '../../style/ListPosts.css'
 import * as Constants from '../../constants'
 import Moment from 'moment'
-import * as BackendAPI from '../../utils/api'
-import * as Actions from '../actions'
+import PostActions from './PostActions'
 
 class ListPosts extends Component {
 
   filter(postCategory,categoryName) {
     if(categoryName === Constants.ALL_POSTS_CATEGORY_NAME) return true
     return postCategory === categoryName
-  }
-
-  upVote(postId) {
-    BackendAPI.voteOnPost(postId,{option:"upVote"})
-    this.props.dispatch(Actions.upVotePost(postId))
-  }
-
-  downVote(postId) {
-    BackendAPI.voteOnPost(postId,{option:"downVote"})
-    this.props.dispatch(Actions.downVotePost(postId))
-  }
-
-  edit(postId) {
-    alert("Editing")
-  }
-
-  delete(postId) {
-    alert("Deleting")
   }
 
   render() {
@@ -59,10 +40,7 @@ class ListPosts extends Component {
                 <div className="divTableCell">{post.author}</div>
                 <div className="divTableCell">{post.voteScore}</div>
                 <div className="divTableCell">
-                  <button onClick={() => this.upVote(post.id)}>Upvote</button>
-                  <button onClick={() => this.downVote(post.id)}>Downvote</button>
-                  <button onClick={() => this.edit(post.id)}>Edit</button>
-                  <button onClick={() => this.delete(post.id)}>Delete</button>
+                  <PostActions postId={post.id}/>
                 </div>
               </div>
             ))}
