b5956d4cc2cc1aa5e3a6ea6e62eba201cb002b9e
chrislzm <me@chrisleung.com>
2017-09-29 17:58:34 -0700

    feat: Categories now navigation bar that highlights current category

---
+++ frontend/src/redux/components/CategoryBar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { capitalize } from '../../utils/helpers'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+const CategoryBar = (props) => {
+  const { categories, currentCategory } = props
+  console.log(currentCategory)
+  return (
+    <ul className="CategoryBar">
+      { categories.map( category => {
+        const liClassName = category.path === currentCategory.path ? "CurrentCategory" : "OtherCategory"
+        return (
+
+        <li key={category.path} className={liClassName}><Link to={'/' + category.path}>{capitalize(category.name)}</Link></li>
+      )}) }
+    </ul>
+  )
+};
+
+const mapStateToProps = (store) => ({
+  currentCategory:store.currentCategory
+})
+
+export default connect(mapStateToProps)(CategoryBar)
