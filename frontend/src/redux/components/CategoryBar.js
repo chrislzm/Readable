3410274cbe33bd1894738876c84632de68a4156e
chrislzm <me@chrisleung.com>
2017-09-29 18:57:21 -0700

    refactor: Using updated variable names

--- frontend/src/redux/components/CategoryBar.js
+++ frontend/src/redux/components/CategoryBar.js
@@ -4,11 +4,10 @@ import { connect } from 'react-redux'
 
 const CategoryBar = (props) => {
   const { categories, currentCategory } = props
-  console.log(currentCategory)
   return (
     <ul className="CategoryBar">
       { categories.map( category => {
-        const liClassName = category.path === currentCategory.path ? "CurrentCategory" : "OtherCategory"
+        const liClassName = category.path === currentCategory.categoryPath ? "CurrentCategory" : "OtherCategory"
         return (
 
         <li key={category.path} className={liClassName}><Link to={'/' + category.path}>{category.name}</Link></li>
