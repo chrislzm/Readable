067d287ec7c157eb4cb46fa88cf30789e2de29bb
chrislzm <me@chrisleung.com>
2017-09-29 19:02:03 -0700

    chore: Updated site name :)

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -3,4 +3,4 @@ export const ALL_POSTS_CATEGORY_NAME = 'All Posts'
 export const DEFAULT_VOTES = 1
 export const DEFAULT_DELETED_FLAG = false
 export const DEFAULT_DATE_FORMAT = "MM-DD-YYYY hh:mm A"
-export const SITE_TITLE = "Welcome to My React+Redux-Powered Forum"
+export const SITE_TITLE = "My React+Redux Powered Forum"
