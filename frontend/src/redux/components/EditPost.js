cdb08462cc18f1ac61b5a9e259dbfa4371b92d1b
chrislzm <me@chrisleung.com>
2017-09-30 18:09:27 -0700

    Initial commit

---
+++ frontend/src/redux/components/EditPost.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { capitalize } from '../../utils/helpers'
+import PostEditor from './PostEditor'
+
+class EditPost extends Component {
+  render() {
+    const {categoryPath, categoryName} = this.props.currentCategory
+    return(
+      <div>
+        <div className="SectionTitle">
+          <h2>
+            Editing Post
+          </h2>
+          <div className="SectionTitleNav">
+            <button>
+              <Link to={ `/${categoryPath}`}>&lt; Back To {capitalize(categoryName)}</Link>
+            </button>
+          </div>
+        </div>
+        <PostEditor categoryPath={categoryPath}/>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  categories: state.categories,
+  currentCategory: state.currentCategory
+});
+
+export default connect(mapStateToProps)(EditPost)
