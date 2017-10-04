94445a93775df8aa247962084bb811a8bfdfdd21
chrislzm <me@chrisleung.com>
2017-10-03 17:48:11 -0700

    refactor: Now using Viewer component

--- frontend/src/redux/components/ListComments.js
+++ frontend/src/redux/components/ListComments.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { addNewComment } from '../actions'
 import * as BackendAPI from '../../utils/api'
-import * as Constants from '../../utils/constants'
-import Moment from 'moment'
+import Viewer from './Viewer'
 
 class ListComments extends Component {
 
@@ -40,32 +39,7 @@ class ListComments extends Component {
       <div>
         <h2>Comments ({numComments})</h2>
         { comments && comments.map(comment => (
-            <div className="divTable blueTable" key={comment.id}>
-              <div className="divTableBody">
-                <div className="divTableRow">
-                  <div className="divTableLabel">Author</div>
-                  <div className="divTableCell">{comment.author}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableLabel">Body</div>
-                  <div className="divTableCell">{comment.body}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableLabel">Date</div>
-                  <div className="divTableCell">{Moment(comment.timestamp, "x").format(Constants.DEFAULT_DATE_FORMAT)}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableLabel">Votes</div>
-                  <div className="divTableCell">{comment.voteScore}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableLabel">Actions</div>
-                  <div className="divTableCell">
-                    Comment actions go here
-                  </div>
-                </div>
-              </div>
-            </div>
+          <Viewer content={comment} key={comment.id}/>
           ))}
         { !comments && (
             <div>This post has no comments.</div>
--- frontend/src/redux/components/ViewPost.js
+++ frontend/src/redux/components/ViewPost.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react'
-import Moment from 'moment'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import * as Constants from '../../utils/constants'
-import PostActions from './PostActions'
 import { capitalize } from '../../utils/helpers'
 import ListComments from './ListComments'
 import LeaveComment from './LeaveComment'
+import Viewer from './Viewer'
 
 class ViewPost extends Component {
   render() {
     const categoryName = this.props.currentCategory.name
     const categoryPath = this.props.currentCategory.path
-    const postId = this.props.match.params.postId
-    const post = this.props.posts[postId]
+    const { postId } = this.props.match.params
+    let post = this.props.posts[postId]
     if(post) {
+      post.id = postId
       post.category = capitalize(post.category)
     }
     return (
@@ -29,32 +28,7 @@ class ViewPost extends Component {
                 </button>
               </div>
             </div>
-            <div className="divTable blueTable">
-              <div className="divTableBody">
-                <div className="divTableRow">
-                  <div className="divTableLabel">Author</div>
-                  <div className="divTableCell">{post.author}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableLabel">Body</div>
-                  <div className="divTableCell">{post.body}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableLabel">Date</div>
-                  <div className="divTableCell">{Moment(post.timestamp, "x").format(Constants.DEFAULT_DATE_FORMAT)}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableLabel">Votes</div>
-                  <div className="divTableCell">{post.voteScore}</div>
-                </div>
-                <div className="divTableRow">
-                  <div className="divTableLabel">Actions</div>
-                  <div className="divTableCell">
-                    <PostActions postId={postId} postTitle={post.title}/>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Viewer content={post}/>
             <LeaveComment postId={postId}/>
             <ListComments parentId={postId}/>
           </div>
