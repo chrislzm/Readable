2ba4bd16f8ef4898d2a36187dffe772fb25f0978
chrislzm <me@chrisleung.com>
2017-10-03 17:34:30 -0700

    doc: Added comment

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ALL_POSTS_CATEGORY_PATH = ''
 export const ALL_POSTS_CATEGORY_NAME = 'All Posts'
-export const DEFAULT_VOTES = 1
+export const DEFAULT_VOTES = 1 // Default # votes for posts and comments
 export const DEFAULT_DELETED_FLAG = false
 export const DEFAULT_DATE_FORMAT = "MM-DD-YYYY hh:mm A"
 export const SITE_TITLE = "My React+Redux Powered Forum"
