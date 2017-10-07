cb94bfa96e457d0f75fc2d065fbdc9ead1dbad29
chrislzm <me@chrisleung.com>
2017-10-06 18:22:39 -0700

    refactor: Converted to functional component

--- frontend/src/redux/components/Viewer.js
+++ frontend/src/redux/components/Viewer.js
@@ -1,48 +1,45 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Actions from './Actions'
 import * as Constants from '../../utils/constants'
 import Moment from 'moment'
 
-class Viewer extends Component {
-
-  render() {
-    const { title, author, body, timestamp, voteScore} = this.props.content
-    let { id, parentId } = this.props.content
-    const { mode } = this.props
-    let commentId
-    if(mode === Constants.ACTIONS_COMMENT_MODE) {
-      commentId = id
-      id = parentId
-    }
-    return (
-      <div className="divTable blueTable">
-        <div className="divTableBody">
-          <div className="divTableRow">
-            <div className="divTableLabel">Author</div>
-            <div className="divTableCell">{author}</div>
-          </div>
-          <div className="divTableRow">
-            <div className="divTableLabel">Body</div>
-            <div className="divTableCell">{body}</div>
-          </div>
-          <div className="divTableRow">
-            <div className="divTableLabel">Date</div>
-            <div className="divTableCell">{Moment(timestamp, "x").format(Constants.DEFAULT_DATE_FORMAT)}</div>
-          </div>
-          <div className="divTableRow">
-            <div className="divTableLabel">Votes</div>
-            <div className="divTableCell">{voteScore}</div>
-          </div>
-          <div className="divTableRow">
-            <div className="divTableLabel">Actions</div>
-            <div className="divTableCell">
-              <Actions postId={id} commentId={commentId} title={title} mode={mode}/>
-            </div>
+function Viewer (props) {
+  const { title, author, body, timestamp, voteScore} = props.content
+  let { id, parentId } = props.content
+  const { mode } = props
+  let commentId
+  if(mode === Constants.ACTIONS_COMMENT_MODE) {
+    commentId = id
+    id = parentId
+  }
+  return (
+    <div className="divTable blueTable">
+      <div className="divTableBody">
+        <div className="divTableRow">
+          <div className="divTableLabel">Author</div>
+          <div className="divTableCell">{author}</div>
+        </div>
+        <div className="divTableRow">
+          <div className="divTableLabel">Body</div>
+          <div className="divTableCell">{body}</div>
+        </div>
+        <div className="divTableRow">
+          <div className="divTableLabel">Date</div>
+          <div className="divTableCell">{Moment(timestamp, "x").format(Constants.DEFAULT_DATE_FORMAT)}</div>
+        </div>
+        <div className="divTableRow">
+          <div className="divTableLabel">Votes</div>
+          <div className="divTableCell">{voteScore}</div>
+        </div>
+        <div className="divTableRow">
+          <div className="divTableLabel">Actions</div>
+          <div className="divTableCell">
+            <Actions postId={id} commentId={commentId} title={title} mode={mode}/>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Viewer;
