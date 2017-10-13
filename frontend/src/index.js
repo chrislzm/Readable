20d331ac6bca952b6420e1a34884c47c07a423b1
chrislzm <me@chrisleung.com>
2017-10-12 20:32:23 -0700

    doc: Added description

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,13 @@
+/*
+  Readable: index.js
+  By Chris Leung
+
+  Description:
+  Renders the application to the DOM. Wraps the app in React Router to keep the
+  application in sync with the URL. Creates a Redux store and passes it to the
+  app via the Provider component.
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
