902120eeefff01d06c8c16217feeb7b815d6664d
chrislzm <me@chrisleung.com>
2017-10-12 20:24:49 -0700

    doc: Updated comments

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -10,12 +10,15 @@
 
 export const SITE_TITLE = "My React+Redux Powered Forum"
 
+// Default category path and name that shows all posts
 export const DEFAULT_CATEGORY_PATH = ''
 export const DEFAULT_CATEGORY_NAME = 'All Posts'
 
+// Date formats used for outputting dates to the user and for validating edited dates
 export const DATE_FORMAT_DISPLAY = "MM-DD-YYYY hh:mm A"
 export const DATE_FORMAT_EDITOR = "MM-DD-YYYY hh:mm:ss.SSS A"
 
+// URL paths to the addPost and edit components
 export const PATH_ADD_POST = "addPost"
 export const PATH_EDIT = "edit"
 
@@ -25,6 +28,7 @@ export const ERROR_MESSAGE_CONTENT_NOT_FOUND = "Not found"
 export const DEFAULT_VOTES = 1
 export const DEFAULT_DELETED_FLAG = false
 
+// Submit button text used in the Edtor component
 export const SUBMIT_BUTTON_TEXT_NEW_POST = "Add Post"
 export const SUBMIT_BUTTON_TEXT_EDIT = "Save Changes"
 export const SUBMIT_BUTTON_TEXT_NEW_COMMENT = "Add Comment"
@@ -33,13 +37,13 @@ export const SUBMIT_BUTTON_TEXT_NEW_COMMENT = "Add Comment"
 export const EDIT_SECTION_TITLE_POST = "Editing Post"
 export const EDIT_SECTION_TITLE_COMMENT = "Editing Comment"
 
-// Edit modes for PostEditor component
+// Edit modes for Editor component
 export const EDITOR_MODE_ADD_POST = "EDITOR_MODE_ADD_POST"
 export const EDITOR_MODE_EDIT_POST = "EDITOR_MODE_EDIT_POST"
 export const EDITOR_MODE_ADD_COMMENT = "EDITOR_MODE_ADD_COMMENT"
 export const EDITOR_MODE_EDIT_COMMENT = "EDITOR_MODE_EDIT_COMMENT"
 
-// Editor error messages
+// Editor component error messages
 export const EDITOR_ERROR_MESSAGE_BLANK_BODY = "Error: Body cannot be blank"
 export const EDITOR_ERROR_MESSAGE_BLANK_AUTHOR = "Error: Author cannot be blank"
 export const EDITOR_ERROR_MESSAGE_BLANK_TITLE = "Error: Title cannot be blank"
@@ -49,11 +53,11 @@ export const EDITOR_ERROR_MESSAGE_INVALID_TIMESTAMP = "Time Stamp must be in a v
 export const CONTENT_MODE_POST = "CONTENT_MODE_POST"
 export const CONTENT_MODE_COMMENT = "CONTENT_MODE_COMMENT"
 
-// Sorting for list posts
+// Sorting options for ListPosts component
 export const LIST_POSTS_SORT_FIELD_TIMESTAMP = "LIST_POSTS_SORT_FIELD_TIMESTAMP"
 export const LIST_POSTS_SORT_FIELD_VOTES = "LIST_POSTS_SORT_FIELD_VOTES"
 
-// CSS constants for sorting direction arrows
+// CSS classes for sorting direction arrow styles
 export const CSS_CLASS_ARROW_UP = "arrow-up"
 export const CSS_CLASS_ARROW_DOWN = "arrow-down"
 export const CSS_CLASS_ARROW_NONE = "arrow-none"
