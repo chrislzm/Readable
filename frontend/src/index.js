93ec35b54c4a452c6f6b347fdefdc2e445f1e757
chrislzm <me@chrisleung.com>
2017-10-12 20:37:49 -0700

    refactor: Removed unused imports, middleware doc: Updated decription

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,28 +3,24 @@
   By Chris Leung
 
   Description:
-  Renders the application to the DOM. Wraps the app in React Router to keep the
-  application in sync with the URL. Creates a Redux store and passes it to the
-  app via the Provider component.
+  Renders the application to the DOM. Creates a Redux store (with Redux DevTools
+  enabled for debugging the Redux store state) and passes the store to the
+  app via the Provider component. Also wraps the app in React Router to keep the
+  application in sync with the URL.
 */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import './style/index.css';
 import App from './redux/components/App';
 import reducer from './redux/reducers/'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
 const store = createStore(
   reducer,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 ReactDOM.render(
