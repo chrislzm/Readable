80e6ebd49ecd3019711490e83936ffa0cd91b59a
chrislzm <me@chrisleung.com>
2017-10-16 15:03:48 -0700

    feat: Added thunk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,15 +12,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
 import './style/index.css';
 import App from './redux/components/App';
 import reducer from './redux/reducers/'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
 )
 
 ReactDOM.render(
