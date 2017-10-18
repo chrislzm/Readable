8647a6d36ab4d8c2f19c74b30347a94e48dd111d
chrislzm <me@chrisleung.com>
2017-10-18 16:29:27 -0700

    style: Reorganized for readability doc: Updated comments

--- frontend/src/api/apiConstants.js
+++ frontend/src/api/apiConstants.js
@@ -7,12 +7,14 @@
   Contains constants used for API method calls in the Readable app.
 */
 
+export const SERVER_URL = 'http://localhost:3001'
+
+// Headers used for all fetch requests
 export const HEADERS = {
   'Content-Type': 'application/json',
   'Authorization': 'whatever-you-want'
 }
 
-export const SERVER_URL = 'http://localhost:3001'
-
+// Valid parameter values for methods used to vote on posts and comments
 export const OPTION_UP_VOTE = 'upVote'
 export const OPTION_DOWN_VOTE = 'downVote'
