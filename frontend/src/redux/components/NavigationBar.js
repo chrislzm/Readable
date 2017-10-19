08ee6a4debc60d38ade554c1006d99fb799f77a1
chrislzm <me@chrisleung.com>
2017-10-18 17:30:30 -0700

    style: Reformatted code for readability doc: Updated description

--- frontend/src/redux/components/NavigationBar.js
+++ frontend/src/redux/components/NavigationBar.js
@@ -4,16 +4,16 @@
 
   Description:
 
-  React component that displays the navigation bar for the Readable app. Lists
-  category names that hyperlink to each category. Gets the current category from
-  the Redux store and highlights it using CSS classes.
+  React stateless functional component that displays the navigation bar for the
+  Readable app. Lists category names that hyperlink to each category. Gets the
+  current category from the Redux store and highlights it using CSS classes.
 
   Props:
-    categories: <Object Array> Required. An array of objects that have two
+    categories: <Array> Required. An array of objects that have two
       properties:
         path: <String> Category path
         name: <String> Category name
-    Redux Store State: Mapped to props
+    currentCategory: <Redux State> Required.
 */
 
 import React from 'react'
@@ -28,8 +28,16 @@ function NavigationBar(props) {
       { categories.map( category => {
         const liClassName = category.path === currentCategory.path ? "CurrentCategory" : "OtherCategory"
         return (
-        <li key={category.path} className={liClassName}><Link to={'/' + category.path}>{category.name}</Link></li>
-      )}) }
+          <li
+            key={category.path}
+            className={liClassName}>
+            <Link
+              to={'/' + category.path}>
+              {category.name}
+            </Link>
+          </li>
+        )
+      }) }
     </ul>
   )
 }
